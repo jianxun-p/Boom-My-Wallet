25f91ed0d76626a3995494af805f30491e64b6f7
jianxun-p <panjianxun88@gmail.com>
2026-03-10 07:53:12 -0400

    update: login page with description

--- Client/src/login/Login.tsx
+++ Client/src/login/Login.tsx
@@ -3,6 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './login.css';
 
+export function Footer() {
+    const ls = { color: '#3182ce' };
+    return <footer className='bg-white'>
+        <p><u><a href="mailto:support@shimaodexibao.dpdns.org" style={ls}>support@shimaodexibao.dpdns.org</a></u></p>
+        <p><u><a href="/terms-of-service.html" style={ls}>Terms of Service</a></u> | <u><a href="/privacy-policy.html" style={ls}>Privacy Policy</a></u></p>
+    </footer>;
+}
+
 export function Login() {
 	
 	const [errMsg, setErrMsg] = useState<string>("");
@@ -18,21 +26,26 @@ export function Login() {
 			window.top.location.href = '/oauth/google/login?' + params.toString();
 	};
 	
-	return (
-		
+	return <>
 		<div className="min-h-screen bg-gray-100 font-sans p-4 sm:p-8 content-center">
 			<div className="max-w-3xl mx-auto bg-white rounded-xl shadow-2xl p-6 sm:p-8">
 			
 				<h1 className="text-3xl sm:text-4xl font-bold text-gray-800 text-center mb-6">
 				Login
 				</h1>
-				<div className="grid sm:grid-rows-1 sm:grid-cols-1 sm:gap-y-4 space-y-4 sm:space-y-0">
+				<div>
+					<p><strong>Boom My Wallet</strong> is a financial management application where YOU OWN YOUR DATA.</p>
+					<p>No more manual financial logging, use Apple Shortcuts with our API endpoints.</p>
+					<br />
+				</div>
+				<div>
 					<button className="login-btn" onClick={googleOauthSignIn}>Sign-In With Google</button>
 				</div>
+				<Footer />
 				<p className='text-red-500'>{errMsg}</p>
 			</div>
 		</div>
-	);
+	</>;
 }
 
 createRoot(document.getElementById('root')!).render(
