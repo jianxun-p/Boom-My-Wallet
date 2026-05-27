aa8d6ccb1176fefed1518c6721b48e430a00deff
jianxun-p <panjianxun88@gmail.com>
2026-05-26 22:13:54 -0400

    fix(api): remove cors allow headers

--- web/Server/server.js
+++ web/Server/server.js
@@ -97,7 +97,6 @@ app.get('/oauth/user', async (req, res, next) => {
 app.use('/api', (_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, OPTIONS, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
